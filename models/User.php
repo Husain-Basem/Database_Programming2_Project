daeb2051760a207eb467a3937d024970faa8bb5b
Nofal-Helal <202001264@student.polytechnic.bh>
2023-05-07 16:02:32 +0300

    Add folder for model classes

---
+++ models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class User
+{
+    /// @var int $uid
+    public $uid;
+    /// @var string
+    public $fname;
+    /// @var string
+    public $lname;
+    /// @var string
+    public $username;
+    /// @var string $password Hashed password
+    public $password;
+    /// @var string
+    public $email;
+    /// @var string
+    public $type;
+    /// @var string $description User description for author types
+    public $description;
+    /// @var string $date Date of registration
+    public $date;
+    /// @var string
+    public $country;
+
+}
--- prelude.php
+++ prelude.php
@@ -8,11 +8,14 @@ error_reporting(E_ALL & ~E_WARNING);
 // Project root i.e. /home/u202001264/DBProject
 define("PROJECT_ROOT", __DIR__);
 
-// Base URL i.e. /~u202001264/DBProject/
+// Base URL i.e. /~u202001264/DBProject
 define("BASE_URL", preg_replace('/\/home\/(\w+)\/public_html\/(.*)/', '/~$1/$2', PROJECT_ROOT));
 
 
-spl_autoload_register(function ($c) {
-    include_once PROJECT_ROOT . "/$c.php";
-});
+// include the database class automatically
+include_once PROJECT_ROOT . "/Database.php";
 
+// include model classes automatically
+spl_autoload_register(function ($c) {
+    include_once PROJECT_ROOT . "/models/$c.php";
+});
