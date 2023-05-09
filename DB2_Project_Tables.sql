bb70ae296fb38e09b6e0479066bd148b38681987
Husain Basem Hasan <92920815+hussainb13@users.noreply.github.com>
2023-05-09 16:34:38 +0300

    Update DB2_Project_Tables.sql
    Syntax and Foreign Keys

--- DB2_Project_Tables.sql
+++ DB2_Project_Tables.sql
@@ -2,17 +2,17 @@ drop table if exists `Users`, `Articles`, `Comments`, `Files`;
 
 CREATE TABLE `Users`
 (
-`uId` int(20) NOT NULL AUTO_INCREMENT ,
-`fName` varchar(250) NOT NULL,
-`lName` varchar(250) NOT NULL,
+`userId` int(20) NOT NULL AUTO_INCREMENT ,
+`firstName` varchar(250) NOT NULL,
+`lastName` varchar(250) NOT NULL,
 `username` varchar(250) NOT NULL,
 `password` varchar(250) NOT NULL,
 `email` varchar(250) NOT NULL,
 `type` varchar(10) NOT NULL,
 `description` varchar(250), 
-`date` varchar(250) NOT NULL,
+`date` date NOT NULL,
 `country` varchar(250) NOT NULL,
-PRIMARY KEY (`uId`)
+PRIMARY KEY (`userId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Articles`
@@ -28,32 +28,33 @@ PRIMARY KEY (`articleId`)
 
 CREATE TABLE `Comments`
 (
-`cId` int(20) NOT NULL AUTO_INCREMENT ,
+`commentId` int(20) NOT NULL AUTO_INCREMENT ,
 `comment` varchar(32000) NOT NULL,
 `rating` varchar(250) NOT NULL,
 `reviewBy` int(20) NOT NULL,
 `date` date NOT NULL,
-PRIMARY KEY (`cId`)
+PRIMARY KEY (`commentId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `Files` (
-`fId` int(20) NOT NULL AUTO_INCREMENT ,
+`fileId` int(20) NOT NULL AUTO_INCREMENT ,
 `fName` varchar(250) NOT NULL,
 `fType` varchar(250) NOT NULL,
 `fLocation` varchar(250) NOT NULL,
 `downloadable` boolean NOT NULL,
 `articleId` int(20) NOT NULL,
-`uId` int(20) NOT NULL,
-PRIMARY KEY (`fId`)
+`userId` int(20) NOT NULL,
+PRIMARY KEY (`fileId`),
+FOREIGN KEY (articleId) REFERENCES Articles(articleId),
+FOREIGN KEY (userId) REFERENCES Users(userId)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
 
-/* In users database 
-	*type of user is categorized into three:
-//-Viewer 
-//-News Writer/Author <- can add a description 
-//-Admin,
-
+/* 
+In Users table database type of user is categorized into three:
+-Viewer
+-News Writer/Author (Can add a description) 
+-Admin
 */
